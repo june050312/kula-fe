47e233d149fd2077eb4998a5e8406ebd633dc4b7
KimTaeho <xoghrla0311@gmail.com>
2024-11-28 19:13:20 +0900

    feat: 검색 기능, 유저 페이지 제작

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/Mainpage';
+import UserLaundarypage from './pages/UserLaundarypage';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <MainPage />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/laundry/:name" element={<UserLaundarypage />} />
+        </Routes>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,18 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './components.css';
 
 function Navbar() {
+  const navigate = useNavigate();
+
   return (
     <nav className="navbar">
+      <div 
+        className="navbar-brand"
+        onClick={() => navigate('/')}
+      >
+        KULA
+      </div>
       <div className="nav-buttons">
         <button className="login-button">로그인</button>
         <button className="signup-button">회원가입</button>
